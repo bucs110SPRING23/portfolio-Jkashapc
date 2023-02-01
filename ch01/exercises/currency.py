f0a80f47f372b9439f466b77d749abb9ea5bd73c
Joey Kashkin <jkashki3@binghamton.edu>
2023-02-01 17:54:02 -0500

    final conversion

--- ch01/exercises/currency.py
+++ ch01/exercises/currency.py
@@ -2,4 +2,8 @@ rate=input("What is the euro to dollar exchnage rate?:")
 amount=input("How much would you like to convert?:")
 rate=float(rate)
 amount=float(amount)
-print(rate*amount)
+total=rate*amount
+total=float(total)
+result=total-3
+result=float(result)
+result=print("Total after Service fee:",result)
