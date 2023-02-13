c0ed58bc69abe86c04a72ba4b1219d0bc9f33460
Joey Kashkin <jkashki3@binghamton.edu>
2023-02-13 15:37:15 -0500

    first tur try

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,14 @@
+import turtle 
+sides=input("How many sides does your desired shape have?:")
+length=input("What should the length of each side be?:")
+float(sides)
+angle=360/sides
+tur=turtle()
+tur.color('blue')
+tur.shape('turtle')
+for i in sides
+tur.down
+tur.forward(length)
+tur.right(angle)
+
+
