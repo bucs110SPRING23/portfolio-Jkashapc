ea21fd011116312539fd754c36583852bb5840cc
Joey Kashkin <jkashki3@binghamton.edu>
2023-02-19 23:49:34 -0500

    hey now

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,5 @@
 import turtle #1. import modules
-import random
+from random import randrange 
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -18,12 +18,30 @@ don.goto(-100,20)
 mic.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-don.down()
-mic.down()
-don.forward(randrange(1,100))
-mic.forward(randrange(1,100))
+#don.down()
+#mic.down()
+#don.forward(randrange(1,100))
+#mic.forward(randrange(1,100))
+#don.up()
+#mic.up()
+#don.goto(-100,20)
+#mic.goto(-100,-20)
+
+for i in randrange(1,10):
+    don.forward(randrange(1,100))
+    mic.forward(randrange(1,100))
+
+
 
 # PART B - complete part B here
+import pygame
+import math 
+
+pygame.init()
+screen=pygame.display.set_mode()
+screen.fill("green")
+pygame.display.flip()
+pygame.draw.polygon(screen, "red", [[100,100],[0,200],[200,200]],25)
 
 
 window.exitonclick()
