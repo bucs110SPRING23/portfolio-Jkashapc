74a17ad9258286c8fc14a5fa1522f874f095f7b2
Joey Kashkin <jkashki3@binghamton.edu>
2023-02-27 12:19:29 -0500

    hey now

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -2,19 +2,25 @@ import pygame
 import random 
 import math
 screen=pygame.display.set_mode()
+ss=pygame.display.get_window_size()
 screen.fill('black')
 pygame.display.flip()
-pygame.draw.circle(screen,'red',(900,600),600)
-pygame.draw.line(screen,'blue',(0,600),(2000,600),5)
-pygame.draw.line(screen,'blue',(900,0),(900,2000),5)
+pygame.draw.circle(screen,'red',(ss[0]/2,ss[1]/2),ss[1]/2)
+pygame.draw.line(screen,'blue',(0,ss[1]/2),(ss[0],ss[1]/2),5)
+pygame.draw.line(screen,'blue',(ss[0]/2,0),(ss[0]/2,ss[1]),5)
 pygame.display.flip()
 pygame.time.wait(3000)
 pygame.display.flip()
-dist_from_center=math.hypot(random.randrange(0,2000), random.randrange(0,2000))
-in_circ= dist_from_center <= 5/2
 for i in range (10):
-    pygame.draw.circle(screen, 'green', (dist_from_center),5)
+    x1=random.randrange(0,ss[0])
+    y1=random.randrange(0,ss[1])
+    dist_from_center=math.hypot(x1-ss[0]/2,y1-ss[1]/2)
+    in_circ = (dist_from_center <= ss[1]/2)
+    if in_circ:
+        pygame.draw.circle(screen, 'green', (x1,y1),5)
+    else:
+        pygame.draw.circle(screen, 'orange', (x1,y1),5)
     pygame.display.flip()
-    break
-pyga,e.time.wait(1000)
+    pygame.time.wait(1000)    
+pygame.time.wait(2000)
 pygame.display.flip()
