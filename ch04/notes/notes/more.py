f725913f02c506e11fef2bad5b0e4ab40d2de100
Joey Kashkin <jkashki3@binghamton.edu>
2023-03-06 17:39:06 -0500

    hae

---
+++ ch04/notes/notes/more.py
@@ -0,0 +1,20 @@
+#singlem responsibility principle- a function should only be responsible for one thing 
+#--functions should only ever do one thing 
+#--a function should never be responsible for input 
+def findmax():
+    max=a 
+    if b > max:
+        max=b 
+    if c > max:
+        max = c 
+    print(max)
+
+for _ in range (3):
+    print("Please enter 3 numbers")
+    a=int(input(":"))
+    b=int(input(":"))
+    c=int(input(":"))
+    findmax()
+
+
+
