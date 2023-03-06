671d8980234fe73b3438224724eda9eab78ca74d
Joey Kashkin <jkashki3@binghamton.edu>
2023-03-06 17:10:23 -0500

    hey niow hey niow

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,23 +1,32 @@
 import turtle
 import random 
 window=turtle.Screen()
+def in_screen (window, t):
+    
 don=turtle.Turtle()
 don.shape('turtle')
 don.color('blue')
-def isInscreen(w, t):
-    if random.random() > 0.1:
-        return True
+don.speed(0)
+dist=10
+angle=90
+in_screen=True
+while in_screen:
+    flip=random.randrange(0,2)
+    if flip:
+        don.right(angle)
     else:
-        return False
+        don.left(angle)
+    don.forward(dist)
 
-while isInscreen(window, don):
-    flip=random.choice['heads','tails']
-    if flip == 'heads':
-        don.right(90)
-        don.forward(50)
-    else:
-        don.left(90)
-        break
-    don.forward(50)
-    
+    donx=don.xcor()
+    dony=don.ycor()
+    xrange=window.window_width()/2
+    yrange=window.window_height()/2
+    don.color(random.choice(colors))
+
+    if abs(donx)>xrange or abs(dony)>yrange:
+        in_screen=False
+
+
+   
 turtle.exitonclick()
