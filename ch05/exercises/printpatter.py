2b5e97b5e4557da2e73951547c6dcc638aabd616
Joey Kashkin <jkashki3@binghamton.edu>
2023-03-13 17:53:18 -0400

    in class 3/13

--- ch05/exercises/printpatter.py
+++ ch05/exercises/printpatter.py
@@ -1,8 +1,11 @@
-n=int(input("how many rows of stars"))
-def starpyr():
-    for i in range(rows):
-        for j in range(i+1):
-            print("* ", end="")
-        print("\n")
+def starpyr(rows):
+    for i in range(1,rows + 1):
+        print("*"*i)
 
-        
+def rstarpyr(rows):
+    for i in range (rows, 0, -1):
+        print("*"*i)
+
+levels=int(input("what height do you want your pyramid to be?:"))
+starpyr(levels)
+rstarpyr(levels)
