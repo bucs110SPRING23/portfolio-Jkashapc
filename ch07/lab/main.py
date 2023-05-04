af66ef82f9ea2352fab9636a56c20d26b0f91a8b
Joey Kashkin <jkashki3@binghamton.edu>
2023-05-04 13:09:43 -0400

    push once more eh

--- ch07/lab/main.py
+++ ch07/lab/main.py
@@ -1,19 +1,19 @@
 def main():
     r = Rectangle(10, 10, 10, 10)
-assert (r-*, [-y, L-height, [-width) - (10,10,10,10))
-r = Rectangle (-1, 1, 1, 1)
-assert((r.x, r.y, r.height, r.width) = (1,1,1,1))
-r = Rectangle(1, -5, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,5,1,1))
-r = Rectangle(1, 1, -10, 1)
-assert((r.x, r.y, r.height, r.width) = (1,1,10,1))
-r = Rectangle(1, 1, 1, -1000)
-asserti(r.x, r.y, r.height, r.width) -*, (1,1,1,1000))
+    assert ((r.x, r.y, r.height, r.width) == (10,10,10,10))
+    r = Rectangle (-1, 1, 1, 1)
+    assert((r.x, r.y, r.height, r.width) == (1,1,1,1))
+    r = Rectangle(1, -5, 1, 1)
+    assert((r.x, r.y, r.height, r.width) == (1,5,1,1))
+    r = Rectangle(1, 1, -10, 1)
+    assert((r.x, r.y, r.height, r.width) == (1,1,10,1))
+    r = Rectangle(1, 1, 1, -1000)
+    assert((r.x, r.y, r.height, r.width) == (1,1,1,1000))
 
 def main():
     s= Surface("myimage.png", 10, 10, 10, 10)
-assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
-srect = s.getRect ()
-assert(srect.X, s.getRect() .y, srect. height, , srect.width) == (10,10, 10, 10) 
-assert S. image 
-print("Test Complete!")
+    assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
+    srect = s.getRect ()
+    assert(srect.X, s.getRect().y, srect.height, srect.width) == (10,10, 10, 10) 
+    assert s.image 
+    print("Test Complete!")
